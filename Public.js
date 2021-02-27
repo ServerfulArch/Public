7ff8486abca607bae5e9584e68053606ca7ff760
QSmally <github@qbot.eu>
2021-02-27 19:58:57 +0100

    Implement recursive directories

--- Public.js
+++ Public.js
@@ -15,28 +15,35 @@ module.exports = (Pathlike, Cache = 300) => {
 
     const Endpoints = new Map();
 
-    for (const Dest of FS.readdirSync(Pathlike)) {
-        if (/^[\._]/.test(Dest)) return;
-        const Endpoint = Path.join(Pathlike, Dest);
+    (function Directory (Destination) {
+        const Source = Path.join(Pathlike, Destination);
+        for (const File of FS.readdirSync(Source)) {
+            if (/^[\._]/.test(File)) return;
+            const SourceFile = Path.join(Source, File);
+            const Endpoint = Path.join(Destination, File);
 
-        const Resource = FS.readFileSync(Endpoint, {
-            encoding: "utf-8"
-        });
-    
-        Endpoints.set(Dest.toLowerCase(), {
-            Resource: Resource instanceof Buffer ?
-                Buffer.from(Resource) : Resource,
-            Type: {
-                js:   "application/javascript",
-                json: "application/json",
-                png:  "image/png",
-                svg:  "image/svg+xml",
-                ico:  "image/x-icon",
-                html: "text/html",
-                css:  "text/css",
-            }[Dest.split(".").pop()]
-        });
-    }
+            if (FS.statSync(SourceFile).isDirectory()) {
+                Directory(Endpoint);
+                continue;
+            }
+
+            const Resource = FS.readFileSync(SourceFile, {encoding: "utf-8"});
+
+            Endpoints.set(Endpoint.toLowerCase(), {
+                Resource: Resource instanceof Buffer ?
+                    Buffer.from(Resource) : Resource,
+                Type: {
+                    js:   "application/javascript",
+                    json: "application/json",
+                    png:  "image/png",
+                    svg:  "image/svg+xml",
+                    ico:  "image/x-icon",
+                    html: "text/html",
+                    css:  "text/css"
+                }[File.split(".").pop()]
+            });
+        }
+    })("/");
 
     return Packet => {
         const URL = Packet.URL.Endpoint.toLowerCase();
